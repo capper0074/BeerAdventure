52a2cf9e215265b77027e95e90b2691b0249dd10
Casper <capper2704@hotmail.dk>
2023-09-29 15:33:32 +0200

    Early start

---
+++ BeerAdventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BeerAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
