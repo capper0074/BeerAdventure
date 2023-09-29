705e2fba3edbea542af58d86fa05ea391b3666cb
Casper <capper2704@hotmail.dk>
2023-09-29 16:15:43 +0200

    Made some more stuff

---
+++ BeerAdventure/Character/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BeerAdventure.Character
+{
+    public static class Player
+    {
+        public static int Stamina { get; set; }
+
+        public static int Health { get; set; }
+
+        public static string Name { get; set; }
+
+        private static bool isInitialize;
+
+        public static void Initialize()
+        {
+            if (isInitialize)
+            {
+                return;
+            }
+            else
+            {
+                Health = 100;
+                Stamina = 100;
+                isInitialize = true;
+            }
+        }
+
+        public static void Display_Stats()
+        {
+            Console.WriteLine("Hi " + Player.Name + " Your current Stamina are: " + Player.Stamina + " And your health are: " + Player.Health);
+        }
+
+        public static void Eat()
+
+
+
+    }
+}
---
+++ BeerAdventure/Stuff/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerAdventure.Stuff
+{
+    public class Item
+    {
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Weight { get; set; }
+
+        public Item(string name, string description, int weight)
+        {
+            Name = name;
+            Description = description;
+            Weight = weight;
+        }
+    }
+}
