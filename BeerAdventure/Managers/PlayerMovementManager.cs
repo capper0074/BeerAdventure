679c7baddeac0223c54490287076a389b7e24be1
Kasper Nissen <KasperNissen1997@gmail.com>
2023-09-30 10:40:45 +0200

    Added a pseudo code comment.

--- BeerAdventure/Managers/PlayerMovementManager.cs
+++ BeerAdventure/Managers/PlayerMovementManager.cs
@@ -10,6 +10,10 @@ namespace BeerAdventure.Managers
 {
     public static class PlayerMovementManager
     {
+        // When a connection is selected, check if it can move there
+        //      If the move is okay, then update the players current section and display the new sections menu via the menumanager
+        //      If the move is not okay, display some sort of error message (Kasper arbejder på noget lækkert her)
+
         public static void MovePlayer(Player player, Section section)
         {
             if (player != null && )
