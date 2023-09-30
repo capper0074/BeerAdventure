c0b1f772c86477b8eeee41eb56d0f3aeab723fef
Kasper Nissen <KasperNissen1997@gmail.com>
2023-09-30 23:24:41 +0200

    Removed some unused files.

--- BeerAdventure/Inventory/WitchInventory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BeerAdventure.Inventory
-{
-    public static class WitchInventory
-    {
-        public static List<Item> SmallItems = new();
-        public static List<Item> MediumItems = new();
-        public static List<Item> LargeItems = new();
-    }
-}
