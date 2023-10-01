9b8ea9bfbcba2544206662bf879d2564f068dc89
Kasper Nissen <KasperNissen1997@gmail.com>
2023-10-01 14:23:23 +0200

    Coins are now subtracted when buying the leather boots from the cobbler.

--- BeerAdventure/Managers/SectionManager.cs
+++ BeerAdventure/Managers/SectionManager.cs
@@ -29,126 +29,6 @@ namespace BeerAdventure.Managers
 
         public static bool IsInitialized { get; private set; } = false;
 
-        #region Old Initialization
-        //    public SectionManager()
-        //    {
-        //        List<Item> items = new();
-        //        {
-        //            //Make items
-        //        }
-
-        //        //Add items to WitchInventory
-        //        foreach(Item i in items)
-        //            if (i.Size == Size.Small)
-        //            {
-        //                WitchInventory.SmallItems.Add(i);
-        //            }
-        //        foreach (Item i in items)
-        //            if (i.Size == Size.Medium)
-        //            {
-        //                WitchInventory.MediumItems.Add(i);
-        //            }
-        //        foreach (Item i in items)
-        //            if (i.Size == Size.Large)
-        //            {
-        //                WitchInventory.LargeItems.Add(i);
-        //            }
-
-        //        Section theMarketSection = new()
-        //        {
-        //            Name = "The Market",
-        //            Description = "This place smells like a rich mix of smelly feet and happy faces.",
-        //        };
-        //        //----------------------------------------------------------------------------------------------------------
-        //        Section theWitchHouseSection = new()
-        //        {
-        //            Name = "The Witch House",
-        //            Description = "A witch reside within this mystic house, who knows what kind of things has happened within these walls.",
-
-        //            Choices = new()
-        //            {
-        //                new Choice()
-        //                {
-        //                    Description = "Talk to the witch. . .",
-        //                    Prerequisites = new()
-        //                    {
-        //                        new()
-        //                        {
-        //                            State = State.HasNotTalkedToWitch,
-        //                            RequiredValue = true
-        //                        }
-        //                    },
-        //                    SuccesfulConsequnce = () =>
-        //                    {
-        //                        Console.WriteLine("Hello young dwarf, i could smell your stinky feet from a horseday away, anyways, welcome to my house, in which i sell potions and magical objects");
-        //                        Thread.Sleep(1000);
-        //                        Console.WriteLine($"What do you want?");
-
-        //                        GameStateManager.SetState(State.HasNotTalkedToWitch, false);
-        //                    },
-        //                    IsVisibleWhen = () => GameStateManager.GetState(State.HasNotTalkedToWitch)
-        //                },
-
-        //                new Choice()
-        //                {
-        //                    Description = "\"I want to see your inventory\"",
-        //                    SuccesfulConsequnce = () =>
-        //                    {
-        //                        Console.WriteLine("Sure have a look. . .");
-        //                        Thread.Sleep(1000);
-        //                        //Show witch inventory
-        //                    },
-        //                    IsVisibleWhen = () => !GameStateManager.GetState(State.HasNotTalkedToWitch)
-        //                }
-        //            },
-        //        };
-        //        //----------------------------------------------------------------------------------------------------------
-        //        Section theShallowMarshSection = new()
-        //        {
-        //            Name = "The Shallow Marsh",
-        //            Description = "Nothing but pests and insects survives more than a week in these parts outside the village.",
-        //        };
-        //        //----------------------------------------------------------------------------------------------------------
-        //        Section homeSection = new()
-        //        {
-        //            Name = "Home",
-        //            Description = "You are home, in your little shack the size of a large horse, it smells like char and rum.",
-        //            Choices = new()
-        //            {
-        //                new Choice()
-        //                {
-        //                    Description = "Search house",
-        //                    Prerequisites = new()
-        //                    {
-        //                        new()
-        //                        {
-        //                            State = State.HasNotSearchedHouse,
-        //                            RequiredValue = true
-        //                        }
-        //                    },
-        //                    SuccesfulConsequnce = () =>
-        //                    {
-        //                        Item coin = new("Rusty coin", "It smells a bit funny, you wonder where it has been. . .", Size.Small);
-
-        //                        Console.WriteLine("You search the house. . .");
-        //                        Thread.Sleep(1000);
-        //                        Console.WriteLine($"You found a coin!");
-
-
-        //                        Inventory.Inventory.SmallItems.Add( coin );
-
-        //                        GameStateManager.SetState(State.HasNotSearchedHouse, false);
-        //                    },
-        //                    IsVisibleWhen = () => GameStateManager.GetState(State.HasNotSearchedHouse)
-        //                }
-        //            },
-        //        };
-        //        //----------------------------------------------------------------------------------------------------------
-        //        //Make connections here
-        //        //Check miro for what connection prerecuisites needs to be made, for an example, the shallow marsh cannot be entered if you are not wearing thick leather boots
-        //    }
-        #endregion
-
         public static void Initialize()
         {
             if (IsInitialized)
@@ -334,6 +214,10 @@ namespace BeerAdventure.Managers
                     Beautifier.DisplayString("You now have a pair of leather boots with high heels!\n", DisplayType.Success);
 
                     GameStateManager.SetState(State.HasShoesOn, true);
+                    Item leatherBoots = new("Old leather boots", "Some old leatherly smelly pissy boots...", Size.Medium);
+
+                    InventoryManager.MediumItems.Add(leatherBoots);
+                    InventoryManager.Coins -= 69; // Nice!
                 },
                 () =>
                 {
@@ -360,12 +244,12 @@ namespace BeerAdventure.Managers
             gardenSection.Connections.Add(new("... go to the smelly puddle of piss and poop. (Swamp)", swampSection));
 
             Choice swampSectionTalkToWitch = new(
-                "... hollar at the crooked backed bitch bog.",
+                "... hollar at the crooked backed witch bog.",
                 () =>
                 {
                     if (!GameStateManager.GetState(State.HasReceivedSecretBushStashHint))
                     {
-                        Beautifier.DisplayString("Your shouts startle the crooked-backed witchybitch in the murky swamp. \n" +
+                        Beautifier.DisplayString("Your shouts startle the crooked-backed witch in the murky swamp. \n" +
                             "She cackles, revealing a toothless grin, and whispers:\n");
                         Beautifier.DisplayString("\"Hidden treasure lies in yonder bush. \n" +
                             "Hush now, and follow the glimmer, little kush.\"\n", DisplayType.Emphasis);
